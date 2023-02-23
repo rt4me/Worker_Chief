6c754daefdff9ea71db62a36c7cec88c459acf55
timb <tabergin@gmail.com>
2023-02-23 17:10:19 -0600

    Added names to blank Medical.txt file. Changed up temp.py to work on reading the stationary files for work assignments and later medical, no-show and annual-reg

--- temp.py
+++ temp.py
@@ -1,7 +1,31 @@
-import sys
-import os
-from pprint import pprint
+import csv
+import logging
 
-#print(sys.path)
-print("")
-print(os.path.dirname(os.path.abspath(__file__)))
+#Set Logging Level to print out
+logging.basicConfig(
+    level = logging.DEBUG, #Levels are DEBUG, INFO, WARNING, ERROR and CRITICAL
+    format = '%(asctime)s [%(filename)s:%(lineno)s - %(funcName)s() ] %(levelname)s %(message)s',
+    datefmt = '%Y-%m-%d %H:%M:%S'
+    #,filename = 'basic.log'
+)
+
+
+def read_locked_work_file(filename = 'DataFiles/Locked_work_assignments.txt'):
+    '''Read the Locked_work_assignment csv file to build a list of locked work assignments (like course_setup and safety)'''
+
+    logging.info('Entering read_locked_work_file')
+    logging.debug(f'Setting up empty work_list inside read_locked_work_file.')
+
+    work_list = []
+    with open(filename, newline = '') as csvfile:
+        file_reader = csv.DictReader(csvfile, delimiter = ',')
+        for row in file_reader:
+            logging.debug(f'row being sent through in for loop: {row}')
+            work_list.append((row['Name'], row['Work_Position']))
+
+    logging.info('Leaving read_locked_work_file')
+    logging.debug(f'Returning work_list: {work_list}')
+
+    return work_list
+
+print(read_locked_work_file())
